f14bb11a42133b6fa195178453b5ba75d2c0a6de
aman tiwari <amantiwari78632@gmail.com>
2021-05-07 14:00:26 +0530

    added exported all compoments

---
+++ client/src/website/components/index.ts
@@ -0,0 +1,11 @@
+export {default as Navbar} from "./Navbar/Navbar";
+export {default as HomeSection} from "./Section/HomeSection";
+export {default as WealthSection} from "./Section/WealthSection";
+export {default as ProjectSection} from "./Section/ProjectSection";
+export {default as PriceSection} from "./Section/PriceSection";
+export {default as Footer} from "./Footer/Footer";
+export {default as Input} from "./Input/Input";
+export {TextArea} from "./Input/Input";
+export {default as FormWrapper} from "./FormWrapper/FormWrapper";
+export {default as ContactCard} from "./ContactCard/ContactCard";
+export {default as FadeInSection} from "./FadeInSection";
