d3ea973be661306c3e2ecb26d2d785ce317db022
aman tiwari <amantiwari78632@gmail.com>
2021-05-07 13:55:52 +0530

    no longer file

--- server/models/User/UserDocument.d.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-import User from "../../../client/src/models/User.d";
-export default interface UserDocument extends User, mongoose.Document {
-    comparePassword: (password: string) => Promise<Boolean>
-}
