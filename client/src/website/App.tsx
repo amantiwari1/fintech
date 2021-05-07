ee0f2db5d3f27f65321a5b4cc73598368dc0c732
aman tiwari <amantiwari78632@gmail.com>
2021-05-07 14:03:15 +0530

    added navbar,  home  , footer

--- client/src/website/App.tsx
+++ client/src/website/App.tsx
@@ -2,17 +2,20 @@ import { ThemeProvider } from './theme/themeContext';
 import GlobalStyles from './theme/globalStyles'
 import { Route, Switch } from "react-router-dom";
 import Home from './pages/Home';
-import Toggle from './theme/themeToggle'
+import { Navbar, Footer } from "../website/components"
+import Contact from './pages/Contact';
 
 
 function App() {
   return (
     <ThemeProvider>
       <GlobalStyles />
-      <Toggle />
+      <Navbar />
       <Switch>
-        <Route exact path="/"  component={Home}  />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/contact" component={Contact} />
       </Switch>
+      <Footer />
     </ThemeProvider>
   );
 }
